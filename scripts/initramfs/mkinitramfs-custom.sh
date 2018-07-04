1d155f71c60c11a670832bb0e10023ee71d9f1f2
root <root@Harley>
2018-07-04 04:50:07 -0400

    Adding missing lib for parted and disable predictable if names

--- scripts/initramfs/mkinitramfs-custom.sh
+++ scripts/initramfs/mkinitramfs-custom.sh
@@ -403,6 +403,7 @@ fi
 echo "Adding all common dependencies"
 cp "${LIB_GNUE}/libparted.so.2" "${DESTDIR}${LIB_GNUE}"
 cp "${LIB_GNUE}/libreadline.so.6" "${DESTDIR}${LIB_GNUE}"
+cp "${LIB_GNUE}/libreadline.so.7" "${DESTDIR}${LIB_GNUE}"
 cp "${LIB_GNUE}/libtinfo.so.5" "${DESTDIR}${LIB_GNUE}"	
 cp "${LIB_GNUE}/libext2fs.so.2" "${DESTDIR}${LIB_GNUE}"
 cp "${LIB_GNUE}/libcom_err.so.2" "${DESTDIR}${LIB_GNUE}"
