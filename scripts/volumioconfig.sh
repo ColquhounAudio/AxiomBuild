29e532ccb5b27624ff8ef1e3a1cb7de2f15681cb
root <ggiraudon@prism19.com>
2018-11-03 03:42:53 -0400

    Disable SSH by default

--- scripts/volumioconfig.sh
+++ scripts/volumioconfig.sh
@@ -296,8 +296,8 @@ ln -s /lib/systemd/system/dynamicswap.service /etc/systemd/system/multi-user.tar
 echo "Adding Iptables Service"
 ln -s /lib/systemd/system/iptables.service /etc/systemd/system/multi-user.target.wants/iptables.service
 
-#echo "Disabling SSH by default"
-#systemctl disable ssh.service
+echo "Disabling SSH by default"
+systemctl disable ssh.service
 echo "Enable AirplayD by default"
 systemctl enable airplay.service
 
